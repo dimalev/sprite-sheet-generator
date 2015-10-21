e2f697f506c155d9d15adf2f7a9db8dc77fd11ab
dima levchenko <ldimat@gmail.com>
2015-10-21 21:48:18 +0300

    more efficient sprite sheet packing, reusing space left after each line packing

--- src/ua/sprites/SpriteSheet.java
+++ src/ua/sprites/SpriteSheet.java
@@ -8,6 +8,7 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.stream.Collectors;
 
 public class SpriteSheet {
@@ -56,15 +57,67 @@ public class SpriteSheet {
   public boolean packInRectangle(Rectangle[] rects, Rectangle target) {
     Arrays.sort(rects, new ByHeightFirst());
 
-    int top = padding, nextTop = padding, left = padding, count = rects.length;
-    for(int i = 0; i < count; ++i) {
+    int top = padding, nextTop = padding, left = padding, count = rects.length, maxHeight = 0, smallest = 0;
+    ArrayList<Rectangle> smallers = new ArrayList<>(16);
+    for(int i = 0; i < count - smallest; ++i) {
       if(left + rects[i].width + padding > target.width) {
+    	if(target.width - left > padding) smallers.add(new Rectangle(left, top, target.width - left, maxHeight));
         top = nextTop;
         left = padding;
+        maxHeight = 0;
+        int leftSmall = smallers.size() - 1;
+    	Collections.reverse(smallers);
+        while(leftSmall >= 0 && i <= count - smallest - 1) {
+        	if(smallers.get(leftSmall).height < rects[count - smallest - 1].height + padding) {
+        		--leftSmall;
+        		continue;
+        	}
+        	int widthSoFar = 0;
+        	Rectangle startRect = new Rectangle(smallers.get(leftSmall));
+        	while(leftSmall >= 0) {
+        		widthSoFar += smallers.get(leftSmall).width;
+        		if(widthSoFar >= rects[count - smallest - 1].width + padding) {
+        			rects[count - smallest - 1].x = startRect.x;
+        			rects[count - smallest - 1].y = startRect.y;
+        			int newWidth = widthSoFar - (rects[count - smallest - 1].width + padding);
+        			smallers.get(leftSmall).x += smallers.get(leftSmall).width - newWidth;
+        			smallers.get(leftSmall).width = newWidth;
+        			if(smallers.get(leftSmall).width <= padding) --leftSmall;
+        			if(startRect.height - (rects[count - smallest - 1].height + padding) > padding) {
+        				++leftSmall;
+        				Rectangle extraRect = new Rectangle(
+								rects[count - smallest - 1].x, 
+								rects[count - smallest - 1].y + rects[count - smallest - 1].height + padding,
+								rects[count - smallest - 1].width + padding,
+								startRect.height - (rects[count - smallest - 1].height + padding)
+								);
+        				if(leftSmall < smallers.size()) smallers.set(leftSmall, extraRect);
+        				else smallers.add(extraRect);
+        			}
+        			++smallest;
+        			break;
+        		}
+        		--leftSmall;
+        	}
+        }
+        smallers.clear();
       }
+      //if(i >= count - smallest - 1) break;
       rects[i].x = left; rects[i].y = top;
       nextTop = Math.max(nextTop, top + rects[i].height + padding);
-      if(nextTop > target.height + padding) return false;
+      if(nextTop > target.height) return false;
+      if(rects[i].height > maxHeight) maxHeight = rects[i].height;
+      else {
+    	  if(maxHeight - rects[i].height - padding > padding) 
+    		  smallers.add(
+    				  new Rectangle(
+    						  rects[i].x, 
+    						  rects[i].y + rects[i].height + padding, 
+    						  rects[i].width + padding, 
+    						  maxHeight - rects[i].height - padding
+    						  )
+    				  );
+      }
       left += rects[i].width + padding;
     }
     return true;
