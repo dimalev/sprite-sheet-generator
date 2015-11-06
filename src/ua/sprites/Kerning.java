56c854cbcff7de634b72b27aea9ec11b99f12546
dima levchenko <ldimat@gmail.com>
2015-11-06 11:58:48 +0200

    kerning file

---
+++ src/ua/sprites/Kerning.java
@@ -0,0 +1,13 @@
+package ua.sprites;
+
+public class Kerning {
+	public final int left;
+	public final int right;
+	public final double kerning;
+	
+	public Kerning(int left, int right, double kerning) {
+		this.left = left;
+		this.right = right;
+		this.kerning = kerning;
+	}
+}
